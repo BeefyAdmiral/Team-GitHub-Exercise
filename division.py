8d0a131a35bc6b5ba0b5bdfddcb7d382004f14e0
Mitchel Hussack <mitchelhussack@gmail.com>
2022-09-21 11:26:50 -0700

    Added unfinished division file

---
+++ division.py
@@ -0,0 +1,27 @@
+nums = []
+finished = False
+
+print("Input any amount of positive integers (End with 0). Enter after each number. This will return the division of each number after the last.")
+
+while (not finished):
+    num = input()
+    if (num.isnumeric()):
+        num = int(num)
+    else:
+        print("Not a valid input. Try again.")
+        continue
+    if (num == 0):
+        finished = True
+    elif (num < 0):
+        print("Not a valid input. Try again.")
+    else:
+        nums.append(num)
+
+def division(ints):
+    div = 1
+    for i in ints:
+        div /= i
+    return div
+
+d = division(nums)
+print("The division of the numbers is {}".format(d))
